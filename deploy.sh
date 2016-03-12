011d538bab52e0fe1286171aeebf550c9df60bc2
Yaxiong Zhao <justicezyx@gmail.com>
2016-03-12 14:01:09 -0800

    Add new files.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+unlink ~/.gitconfig
+ln -s -f $(pwd)/gitconfig ~/.gitconfig
+
+unlink ~/.gitignore_global
+ln -s -f $(pwd)/gitignore_global ~/.gitignore_global
+
+unlink ~/.vimrc
+ln -s -f $(pwd)/vimrc ~/.vimrc
+
+unlink ~/.zshrc
+ln -s -f $(pwd)/zshrc ~/.zshrc
+
+unlink ~/.vimrcs
+ln -s -f $(pwd)/vimrcs ~/.vimrcs
+
+unlink ~/.zshrcs
+ln -s -f $(pwd)/zshrcs ~/.zshrcs
+
+unlink ~/.tmux.conf
+ln -s -f $(pwd)/tmux.conf ~/.tmux.conf
